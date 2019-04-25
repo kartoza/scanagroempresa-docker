d2e056f39f5ef751ad1feaaa32a56784109e4beb
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2019-04-25 20:51:15 +0700

    Add private theme app handling with ssh key

--- deployment/production/docker/geonode_generic/install-theme-app.sh
+++ deployment/production/docker/geonode_generic/install-theme-app.sh
@@ -2,6 +2,17 @@
 
 echo "Checking Geonode custom theme app settings"
 
+if [ ! -z "$GEONODE_THEME_APP_SSH_KEY" ]; then
+
+	echo "Install SSH key for theme app"
+
+	mkdir -p ~/.ssh
+	chmod 700 ~/.ssh
+	cat ${GEONODE_THEME_APP_SSH_KEY} > ~/.ssh/id_rsa
+	chmod 600 ~/.ssh/id_rsa
+
+fi
+
 if [ ! -z "$GEONODE_THEME_APP_PIP_URL" ]; then
 
 	echo "Install Geonode custom theme: $GEONODE_THEME_APP_PIP_URL"
