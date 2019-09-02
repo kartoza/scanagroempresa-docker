b4a2876f69182253b0471c08257f12de68e423cb
Pierre-Alix Tremblay <pierrealix@kartoza.com>
2019-09-02 13:01:16 +0200

    Added overrided files for production

--- deployment/production/docker/geonode_generic/overrides-geonode.sh
+++ deployment/production/docker/geonode_generic/overrides-geonode.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Overiding geoserver/helpers.py"
-cp /usr/src/scanagroempresa-docker/core/overrides/geonode/geonode/geoserver/helpers.py /usr/src/geonode/geonode/geoserver/helpers.py
+echo "Overriding geoserver/helpers.py"
+cp /home/web/django_project/core/overrides/geonode/geonode/geoserver/helpers.py /usr/src/geonode/geonode/geoserver/helpers.py
+
+echo "Overriding geonode/geonode/static/geonode/js/utils/utils.js"
+cp /home/web/django_project/core/overrides/geonode/geonode/static/geonode/js/utils/utils.js /usr/src/geonode/geonode/static/geonode/js/utils/utils.js
+
+echo "Overriding geonode/geonode/layers/templates/upload/layer_upload.html"
+cp /home/web/django_project/core/overrides/geonode/geonode/layers/templates/upload/layer_upload.html /usr/src/geonode/geonode/layers/templates/upload/layer_upload.html
