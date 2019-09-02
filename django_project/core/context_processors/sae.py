e12452d7aeb20660331ff906f220375f0a8e9324
Pierre-Alix Tremblay <pierrealix@kartoza.com>
2019-09-02 12:18:12 +0200

    Got csrftoken based from CSRF_COOKIE_NAME django env variable

--- django_project/core/context_processors/sae.py
+++ django_project/core/context_processors/sae.py
@@ -4,7 +4,8 @@ from django.conf import settings
 def sae_context(request):
     defaults = dict(
         SCANAGROEMPRESA_URL=settings.SCANAGROEMPRESA_URL,
-        GEOSERVER_PUBLIC_LOCATION=settings.GEOSERVER_PUBLIC_LOCATION
+        GEOSERVER_PUBLIC_LOCATION=settings.GEOSERVER_PUBLIC_LOCATION,
+        CSRF_COOKIE_NAME=settings.CSRF_COOKIE_NAME
     )
-    
+
     return defaults
