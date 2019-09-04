e4291dbb99974780005d6fdf23033977d1cea525
Pierre-Alix Tremblay <pierrealix@kartoza.com>
2019-09-04 12:56:17 +0200

    get CSRF_COOKIE_NAME

--- deployment/docker-geonode/overrides-geonode.sh
+++ deployment/docker-geonode/overrides-geonode.sh
@@ -8,3 +8,6 @@ cp /home/web/django_project/core/overrides/geonode/geonode/static/geonode/js/uti
 
 echo "Overriding geonode/geonode/layers/templates/upload/layer_upload.html"
 cp /home/web/django_project/core/overrides/geonode/geonode/layers/templates/upload/layer_upload.html /usr/src/geonode/geonode/layers/templates/upload/layer_upload.html
+
+echo "Overriding geonode/geonode/templates/_permissions_form_js.html"
+cp /home/web/django_project/core/overrides/geonode/geonode/templates/_permissions_form_js.html /usr/src/geonode/geonode/templates/_permissions_form_js.html
